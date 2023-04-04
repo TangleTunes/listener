b2315267aef6484a28dad547c109e1c17d5e66e8
EvanaR <57092826+EvanaR@users.noreply.github.com>
2023-04-04 12:36:57 +0200

    fixed text overlow issues and added responsiveness to profile page

--- lib/screens/discovery.dart
+++ lib/screens/discovery.dart
@@ -300,46 +300,51 @@ class _SongItemState extends State<SongItem> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        widget.song.songName,
-                        style: const TextStyle(
-                          color: COLOR_PRIMARY,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 8, 10, 8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          widget.song.songName,
+                          style: const TextStyle(
+                            color: COLOR_PRIMARY,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        'Artist: ${widget.song.artist}',
-                        style: const TextStyle(
-                          color: COLOR_PRIMARY,
+                        Text(
+                          overflow: TextOverflow.ellipsis,
+                          'Artist: ${widget.song.artist}',
+                          style: const TextStyle(
+                            color: COLOR_PRIMARY,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Duration: ${formatedTime(widget.song.duration)}',
-                        style: const TextStyle(
-                          color: COLOR_PRIMARY,
+                        Text(
+                          overflow: TextOverflow.ellipsis,
+                          'Duration: ${formatedTime(widget.song.duration)}',
+                          style: const TextStyle(
+                            color: COLOR_PRIMARY,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Price: ${priceInMiotaPerMinute(widget.song.price, widget.song.duration, widget.song.byteSize)} MIOTA/min',
-                        style: const TextStyle(
-                          color: COLOR_PRIMARY,
+                        Text(
+                          overflow: TextOverflow.ellipsis,                          
+                          'Price: ${priceInMiotaPerMinute(widget.song.price, widget.song.duration, widget.song.byteSize)} MIOTA/min',
+                          style: const TextStyle(
+                            color: COLOR_PRIMARY,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
